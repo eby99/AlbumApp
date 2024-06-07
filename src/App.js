2ab9945cb2837822e9d9e2d5ac541066da6df406
eby99 <ebymathew619@gmail.com>
2024-06-08 00:08:50 +0530

    AlbumApp Compelted

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Add from './components/Add';
+import Delete from './components/Delete';
+import Searchx from './components/Searchx';
+import View from './components/View';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Add/>}/>
+          <Route path='/Delete' element={<Delete/>}/>
+          <Route path='/Search' element={<Searchx/>}/>
+          <Route path='/View' element={<View/>}/>
+         
+          
+
+        </Routes>
+      </BrowserRouter>
   );
 }
 
